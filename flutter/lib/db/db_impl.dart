a8f138f61bb683230483e5c8849c361dc6c1f1ce
hatano0x06 <siroyagitegamitabeta@gmail.com>
2022-03-09 16:59:48 +0900

    dummyDb

--- flutter/lib/db/db_impl.dart
+++ flutter/lib/db/db_impl.dart
@@ -1,6 +1,9 @@
 
 // コマデータ
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:tateyomi_gigarizer/model/frame_image.dart';
 import 'package:tateyomi_gigarizer/model/project.dart';
 
 class DbImpl{
@@ -20,5 +23,33 @@ class DbImpl{
 
     ];
   }
+  Future<String> insertProject(Project _insertProj) async {
+    _insertProj.dbIndex = DateTime.now().millisecondsSinceEpoch.toString() + "_proj";
+    return _insertProj.dbIndex;
+  }
+  Future<void> updateProject(Project _updateProj) async { }
 
+
+
+  Future<List<FrameImage>> getFrameList(Project _proj) async {
+    return [
+      FrameImage(
+        dbIndex     : "aaaa",
+        project     : _proj,
+        dbInstance  : this,
+        byteData    : null, 
+        name        : "aaa",
+        angle       : 0,
+        sizeRate    : 1.0,
+        position    : const Point<double>(0,0),
+        size        : const Point(200,200),
+      )
+    ];
+  }
+  Future<String> insertFrame(FrameImage _insertFrame) async {
+    _insertFrame.dbIndex = DateTime.now().millisecondsSinceEpoch.toString() + "_frame";
+    return _insertFrame.dbIndex;
+  }
+
+  Future<void> updateFrame(FrameImage _updateFrame) async { }
 }
