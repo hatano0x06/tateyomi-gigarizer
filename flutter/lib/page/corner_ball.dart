5e354bff6d8ee383bfd8c3a80b4c95525c95a110
hatano0x06 <siroyagitegamitabeta@gmail.com>
2022-03-09 15:27:00 +0900

    空白

--- flutter/lib/page/corner_ball.dart
+++ flutter/lib/page/corner_ball.dart
@@ -2,57 +2,57 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class CornerBallWidget extends StatefulWidget {
-    final Function() onDragStart;
-    final Function(Offset) onDrag;
-    final Function() onDragEnd;
-    final double ballDiameter;
-    final SystemMouseCursor cursor;
+  final Function() onDragStart;
+  final Function(Offset) onDrag;
+  final Function() onDragEnd;
+  final double ballDiameter;
+  final SystemMouseCursor cursor;
 
-    const CornerBallWidget({
-      Key? key, 
-      required this.onDragStart, 
-      required this.onDrag, 
-      required this.onDragEnd, 
-      required this.ballDiameter, 
-      required this.cursor, 
-    }):super(key:key);
+  const CornerBallWidget({
+    Key? key, 
+    required this.onDragStart, 
+    required this.onDrag, 
+    required this.onDragEnd, 
+    required this.ballDiameter, 
+    required this.cursor, 
+  }):super(key:key);
 
 
-    @override
-    _CornerBallWidgetState createState() => _CornerBallWidgetState();
+  @override
+  _CornerBallWidgetState createState() => _CornerBallWidgetState();
+}
+
+class _CornerBallWidgetState extends State<CornerBallWidget> {
+  _handleStart(DragStartDetails details) {
+    widget.onDragStart();
   }
 
-  class _CornerBallWidgetState extends State<CornerBallWidget> {
-    _handleStart(DragStartDetails details) {
-      widget.onDragStart();
-    }
+  _handleUpdate(DragUpdateDetails details) {
+    widget.onDrag(Offset(details.globalPosition.dx, details.globalPosition.dy - kToolbarHeight));
+  }
 
-    _handleUpdate(DragUpdateDetails details) {
-      widget.onDrag(Offset(details.globalPosition.dx, details.globalPosition.dy - kToolbarHeight));
-    }
+  _handleEnd(DragEndDetails details) {
+    widget.onDragEnd();
+  }
 
-    _handleEnd(DragEndDetails details) {
-      widget.onDragEnd();
-    }
-
-    @override
-    Widget build(BuildContext context) {
-      return MouseRegion(
-        cursor  : widget.cursor,
-        child   : GestureDetector(
-          onPanStart  : _handleStart,
-          onPanUpdate : _handleUpdate,
-          onPanEnd    : _handleEnd,
-          child: Container(
-            width: widget.ballDiameter,
-            height: widget.ballDiameter,
-            decoration: const BoxDecoration(
-              color: Colors.blue,
-              // color: Colors.blue.withOpacity(200),
-              shape: BoxShape.circle,
-            ),
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      cursor  : widget.cursor,
+      child   : GestureDetector(
+        onPanStart  : _handleStart,
+        onPanUpdate : _handleUpdate,
+        onPanEnd    : _handleEnd,
+        child: Container(
+          width: widget.ballDiameter,
+          height: widget.ballDiameter,
+          decoration: const BoxDecoration(
+            color: Colors.blue,
+            // color: Colors.blue.withOpacity(200),
+            shape: BoxShape.circle,
           ),
         ),
-      );
-    }
+      ),
+    );
   }
+}
